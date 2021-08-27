148c5d46d3e9e6ea5128db72fcb0a02fc97636b7
Bespalov Alexey <bespalov.alexey@yandex.ru>
2021-08-27 07:42:33 +0700

    add rm kde source folder

--- rsync-slackware-current_kde.sh
+++ rsync-slackware-current_kde.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+rm -r slackware64-current/source/kde/
 mkdir -p ./slackware64-current/source/kde/
 rsync -av rsync://mirror.yandex.ru/slackware/slackware64-current/source/kde/ ./slackware64-current/source/kde/
 #http://slackware.uk/slackware/slackware64-current/source/kde/kde/
