639634fc58842ce85886743c213c4b1228ca5664
Bespalov Alexey <bespalov.alexey@yandex.ru>
2021-09-03 21:25:43 +0700

    make slackware-cutefis-settings /etc/skel

--- make-plasma5-min-iso.sh
+++ make-plasma5-min-iso.sh
@@ -2,7 +2,8 @@
 
 cp setpkg-cutefish-kdemin slackware64-current/isolinux/setpkg
 cp message.txt slackware64-current/isolinux/
-
+cp -r pkg/cutefish/ slackware64-current/slackware64/
+cp -r pkg/extra/ slackware64-current/slackware64/
 
 xorriso -as mkisofs \
   -iso-level 4 \
