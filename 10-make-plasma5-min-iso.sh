2db6790b7d5a151db4f0bdb3a020ff202803880c
Bespalov Alexey <bespalov.alexey@yandex.ru>
2021-09-12 19:55:45 +0700

    Moved coping files

--- 10-make-plasma5-min-iso.sh
+++ 10-make-plasma5-min-iso.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cp setpkg-cutefish-kdemin slackware64-current/isolinux/setpkg
-cp message.txt slackware64-current/isolinux/
-cp -r pkg/cutefish/ slackware64-current/slackware64/
-cp -r pkg/extra/ slackware64-current/slackware64/
-
 xorriso -as mkisofs \
   -iso-level 4 \
   -full-iso9660-filenames \
