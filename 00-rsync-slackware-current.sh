8cb0b070c7ac6f3cde58585aff061514b7423bdc
Bespalov Alexey <bespalov.alexey@yandex.ru>
2021-09-12 19:39:51 +0700

    changed mirror to slackware

--- 00-rsync-slackware-current.sh
+++ 00-rsync-slackware-current.sh
@@ -1,4 +1,7 @@
 rm -r -f slackware64-current
 #mkdir -p slackware64-current
 
-rsync -av --exclude source/ rsync://mirror.yandex.ru/slackware/slackware64-current/ ./slackware64-current/
+SLACKREPO=mirrors.slackware.com/slackware/slackware64-current/
+#SLACKREPO=mirror.yandex.ru/slackware/slackware64-current/
+
+rsync -av --exclude source/ rsync://$SLACKREPO ./slackware64-current/
