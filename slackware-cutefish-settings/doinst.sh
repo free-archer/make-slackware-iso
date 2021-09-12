d6e692d1b9aad2e2dca503232ee68e6bc9d33b69
Bespalov Alexey <bespalov.alexey@yandex.ru>
2021-09-12 09:17:48 +0700

    added a raplacment runlevel to 4

--- slackware-cutefish-settings/doinst.sh
+++ slackware-cutefish-settings/doinst.sh
@@ -1,1 +1,12 @@
- 
+#inittab set runlavel 4
+RUNLEVEL=4
+sed -i /etc/inittab -e "s/\(id:\).\(:initdefault:\)/\1${RUNLEVEL}\2/"
+
+#gxkb
+#cp /usr/share/applications/gxkb.desktop /etc/xdg/autostart/
+#cp /usr/share/applications/gxkb.desktop $XDG_CONFIG_HOME/autostart/
+
+#chmod +x /etc/xdg/autostart/gxkb.desktop
+#chmod +x $XDG_CONFIG_HOME/autostart/gxkb.desktop
+
+
